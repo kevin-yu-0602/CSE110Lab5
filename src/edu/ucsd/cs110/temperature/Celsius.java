b8cde0e3d04f122cb45b2953a8d128c56d498098
Kevin Yu <kevin_yu_0602@hotmail.com>
2018-05-02 11:49:54 -0700

    Adde dtravis

---
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -0,0 +1,23 @@
+package edu.ucsd.cs110.temperature;
+public class Celsius extends Temperature
+{
+    public Celsius(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        return this;
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return new Fahrenheit((9*getValue()/5)+32);
+    }
+
+    public String toString()
+    {
+        return "" + getValue() + " C";
+    }
+}
